a6fc452213a90761b1228baa4f05b4cc91257b55
Bk <92112028+BluAuraX@users.noreply.github.com>
2024-02-29 12:14:10 -0700

    Spiritual Stones

---
+++ Sandbox23_Bk/Assets/Scenes/SpiritualStonesExercise/Scripts/StoneSocket.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+
+
+    public class StoneSocket : MonoBehaviour
+    {
+        protected static int stoneCount;
+
+        [Tooltip("Drag one of the three spiritual stones in here")] //This allows us to display some text when hovering over the variable name in the editor.
+        [SerializeField] private GameObject stoneReference;
+
+
+        private void OnTriggerEnter(Collider other)
+        {
+            stoneCount++;
+            Debug.Log($"{other.gameObject.name} gameobject entered the trigger");
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            stoneCount--;
+            Debug.Log($"{other.gameObject.name} gameobject exited the trigger");
+        }
+}
+
+
