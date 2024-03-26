91c4a7af5ac597d27dd98d92162f574ef3612ba8
Bk <92112028+BluAuraX@users.noreply.github.com>
2024-03-26 16:53:48 -0600

    Animation Show

---
+++ Sandbox23_Bk/Assets/Scenes/AnimationShow_Bk/Scripts/AnimControl.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimControl : MonoBehaviour
+{
+    public GameObject bruce;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("F"))
+        {
+            bruce.GetComponent<Animator>().Play("Flying Bicycle Kick");
+        }
+        if (Input.GetButtonDown("G"))
+        {
+            bruce.GetComponent<Animator>().Play("Mma Kick");
+        }
+        if (Input.GetButtonDown("H"))
+        {
+            bruce.GetComponent<Animator>().Play("Hurricane Kick");
+        }
+
+    }
+}
